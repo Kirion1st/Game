3f99674596404d0189e5568e18a1a1881a617239
Kirion I <samweisgamdschie123@gmail.com>
2020-03-30 02:05:11 +0200

    added a triangle based on the GameEngine

--- src/de/kirion/game/Tutorial.java
+++ src/de/kirion/game/Tutorial.java
@@ -1,6 +1,7 @@
 package de.kirion.game;
 
 import de.kirion.gameEngine.Game;
+import de.kirion.gameEngine.rendering.Mesh;
 
 /**
  * 
@@ -9,6 +10,8 @@ import de.kirion.gameEngine.Game;
  */
 public class Tutorial extends Game {
 
+	private Mesh triangle;
+	
 	public Tutorial(int width, int height, String windowName) {
 		super(width, height, windowName);
 	}
@@ -16,5 +19,20 @@ public class Tutorial extends Game {
 	public static void main(String[] args) {
 		Tutorial t = new Tutorial(800, 600, "Game Window");
 		t.run();
+	}
+	
+	@Override
+	public void init() {
+		triangle = new Mesh(new float[] {-0.5f, -0.5f, 1.0f, 0.5f, -0.5f, 1.0f, 0, 0.5f, 1.0f} );
+	}
+	
+	@Override
+	public void render() {
+		triangle.render();
+	}
+	
+	@Override
+	public void release() {
+		triangle.release();
 	}
 }
